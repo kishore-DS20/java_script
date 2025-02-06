9c211e94bf71be05f339d7a1d7dfc1ad0250eac3
kishore-DS20 <kishore.s@datasirpi.com>
2025-02-06 18:40:07 +0530

    added comment

--- Feb-6/array.js
+++ Feb-6/array.js
@@ -1,2 +1,93 @@
+// Array:
+
 // Array=[1,2,3,4,5];
-// console.log(Array)
+// console.log(Array);
+
+// let arr=[3,4,5,'a',"abc",[3,4]]
+// console.log(arr[4][1]);
+
+// 2D arrays:
+// let matrix = [[1,2,3],[4,5,6],[6,7,8]];
+// console.log(matrix[1][2])
+
+// // Array Methods:
+// let array = ['b','c','d','e','f']
+
+// array.push('f')
+// console.log(array)
+
+// console.log(array.pop())
+
+// console.log(array.shift())
+// console.log(array.unshift('aa','bb'))
+// console.log(array)
+
+// delete array[2]
+// console.log(array)
+
+
+// let array = ['a','b','c','d','e','f']
+// array.splice(3,3,'a','b','c')
+// console.log(array)
+
+// array.splice(1,0,'a')
+// console.log(array)
+
+// console.log(array.reverse( ))
+
+// let str = array.join()
+// console.log(str)
+
+// let str3 ="a,s,d,f,g,h,j,k,l";
+// console.log(str3.split(-));  
+
+// let firstarr = [1,2,3];
+// let secondarr = [4,5,6];
+// let joinedarr = [firstarr+secondarr]
+// console.log(joinedarr);
+
+// let firstarr = [1,2,3];
+// let secondarr = [4,5,6];
+
+// let joinedarr = firstarr.concat(secondarr)
+// console.log(joinedarr)
+
+// Exercises of Array:
+
+// let arr = [5,8,10,7,9,11];
+// arr.splice(3,3,17,19,111);
+// console.log(arr);
+// arr.unshift(100);
+// console.log(arr);
+// arr.push(200);
+// console.log(arr);
+// arr.push(1000);
+// console.log(arr);
+// console.log(arr.length/2);
+
+// console.log(arr.splice(5,0,0));
+// // console.log(array)
+// // console.log(arr);
+// let result=arr.splice(5,0,0)
+
+// console.log(result);
+
+
+
+// let arr = [5,8,10,7,9,11];
+//  let value=Math.floor(arr.length/2);
+//  console.log(arr.splice(value,0,0));
+
+// insert zero in the middle:
+
+// let arr = [5,8,10,7,9,11];
+// arr.splice(3,3,17,19,111);
+// console.log(arr);
+// arr.unshift(100);
+// console.log(arr);
+// arr.push(200);
+// console.log(arr);
+// arr.push(1000);
+// console.log(arr);
+// arr.splice(5,0,0)
+// console.log(arr);
